861213e33122a53ae7a7d114740f00ddb6cc0311
k-kamijima77 <k-kamijima095@yahoo.co.jp>
2020-12-17 11:21:53 +0900

    first commit

---
+++ b.rb
@@ -0,0 +1,8 @@
+score = 80
+if score >= 70
+  puts "合格"
+elsif score >= 50
+    puts "再試"
+else 
+    puts "不合格"
+end
---
+++ c.rb
@@ -0,0 +1,11 @@
+for a  in 1..100
+  if a % 15 == 0
+     puts "FIZZ BUZZ"
+  elsif a % 5 == 0
+     puts "BUZZ"
+  elsif a % 3 == 0
+     puts "FIZZ"
+  else 
+     puts a
+   end
+end
